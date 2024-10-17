c162fc88ca931e93a95c255d7cb689153e9df1c5
Lumpysun <ispencerstaarv@gmail.com>
2024-10-17 10:14:36 +0100

    Added comments

--- assignment/task1.py
+++ assignment/task1.py
@@ -3,13 +3,21 @@ import math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+# Define J (sqrt of -1)
 j = cmath.sqrt(-1)
 
+# Define equation for the initial position of the inspector
 z_initial = (1+j) / ((math.sqrt(2) * j**4)) * (((1+j) / np.absolute(1+j))**1)
 
+# Define variable u to use in iteration
 u = (math.sqrt(2) / 1 + j)
 
 def move(z_initial):
+    '''
+    funtion used to move the inspector by iterating through positions
+    Arguments:
+    z_initial - float
+    '''
     for move in range(1, 64):
         print(z_initial)
         z_initial = (z_initial) / u
