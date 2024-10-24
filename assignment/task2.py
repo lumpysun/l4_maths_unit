ad4ce819f81f93e6bcc73093ef574dd8f81921f1
Lumpysun <ispencerstaarv@gmail.com>
2024-10-24 12:38:58 +0100

    Added task 2

--- assignment/task2.py
+++ assignment/task2.py
@@ -4,4 +4,29 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 j = cmath.sqrt(-1)
+u = (math.sqrt(2) / 1 + j)
+
+x, y = 0, 0j
+
+z_initial = ((math.sqrt(2) * (1 + j))/ (((1 + j) / (math.sqrt(2)))**1))
+
+
+i = 0
+print(int(z_initial.real), z_initial.imag)
+while int(z_initial.real) != x or int(z_initial.imag) != y:
+    i += 1
+    z_initial = (z_initial) / u
+    plt.plot(z_initial.real, z_initial.imag, 'ro')
+    print(i, z_initial.real, z_initial.imag)
+
+x_label= 'Real'; y_label='Imaginary'
+plt.xlabel(x_label)
+plt.ylabel(y_label)
+plt.xlim(-2, 2)
+plt.ylim(2,-2)
+plt.grid()
+plt.show()
+
+
+
 
