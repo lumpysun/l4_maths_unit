03e3204c0f040bae1a13402882531aa348b37d08
Alister <alister189@gmail.com>
2024-10-31 12:30:11 +0000

    Conflict Time Baby

---
+++ assignment/task4.py
@@ -0,0 +1,23 @@
+import cmath; import numpy as np
+import math
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+j = cmath.sqrt(-1)
+e = np.exp(1)
+a = 1
+
+x = np.linspace(-3,3,1000)
+y = np.sqrt((x*(x-1)**2)/(x-2)**2)
+
+
+
+plt.plot(x,y,'g:')
+
+x_label= 'x'; y_label='y'
+plt.xlabel(x_label)
+plt.ylabel(y_label)
+plt.xlim(-3,3 )
+plt.ylim(-3, 3)
+plt.grid()
+plt.show()
